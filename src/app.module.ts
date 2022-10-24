725e198ed787b6494c7310b0279a3942f165df06
Marius STEPHANY <marius.stephany@gmail.com>
2022-10-24 17:49:07 +0200

    :sparkles: Add user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TrackerModule } from './tracker/tracker.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TrackerModule],
+  imports: [TrackerModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
