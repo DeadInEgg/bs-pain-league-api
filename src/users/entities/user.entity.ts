3f606aab38a8ffb08ce32322e9c2e47813c6f02e
Marius STEPHANY <marius.stephany@gmail.com>
2022-10-30 18:36:20 +0100

    :sparkles: Add created and updated dates on User

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,12 @@
 import { Tracker } from 'src/trackers/entities/tracker.entity';
-import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -17,4 +24,17 @@ export class User {
 
   @OneToMany((type) => Tracker, (tracker) => tracker.id)
   trackers: Tracker[];
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  public created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  public updated_at: Date;
 }
