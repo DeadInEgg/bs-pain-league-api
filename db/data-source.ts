add8c4a4db7f2e6358526c46bb1be919de1fd901
Marius STEPHANY <marius.stephany@gmail.com>
2022-11-06 16:50:22 +0100

    :package: Initialize migrations

---
+++ db/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+
+config();
+
+const configService = new ConfigService();
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*.js'],
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Tracker } from './trackers/entities/tracker.entity';
 import { TrackersModule } from './trackers/trackers.module';
-import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { GamesModule } from './games/games.module';
-import { Game } from './games/entities/game.entity';
-import { Map } from './games/entities/map.entity';
-import { Type } from './games/entities/type.entity';
-import { Mode } from './games/entities/mode.entity';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [User, Tracker, Game, Map, Type, Mode],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     TrackersModule,
     UsersModule,
     AuthModule,
