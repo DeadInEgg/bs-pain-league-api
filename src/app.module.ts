fa371c338f9de4602c08b17bf1eb03d85894c660
Joassy <maximejoassy@hotmail.fr>
2022-11-26 15:41:42 +0100

    :sparkles: Add db-test. Add .env.dev and .env.test

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,22 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { GamesModule } from './modules/games/games.module';
-import { dataSourceOptions } from './data-source';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
     TypeOrmModule.forRoot({
-      ...dataSourceOptions,
-      autoLoadEntities: true,
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as unknown as number,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      synchronize: process.env.NODE_ENV === 'test',
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/migrations/*.js'],
     }),
     TrackersModule,
     UsersModule,
@@ -22,4 +30,5 @@ import { dataSourceOptions } from './data-source';
   controllers: [],
   providers: [],
 })
+
 export class AppModule {}
